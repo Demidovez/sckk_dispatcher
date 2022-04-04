b34a809cdff2cee86afd152b944e194bed272ba5
Demidovez <nikolaydemidovez@example.com>
2022-04-04 14:26:53 +0300

    corrected api

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,14 +2,12 @@ export const formatNumber = (num) =>
   num ? num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ") : num;
 
 export const getApiUrl = () => {
-  const API_PORT = process.env.REACT_APP_API_PORT;
-
   let api_url = "";
 
   if (process.env.NODE_ENV === "development") {
     api_url = process.env.REACT_APP_API_DEV_HOST;
   } else {
-    api_url = "http://" + window.location.hostname + ":" + API_PORT;
+    api_url = process.env.REACT_APP_API_HOST;
   }
 
   return api_url;
